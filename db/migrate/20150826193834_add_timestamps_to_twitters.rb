d864de923e5ee33d217c40ba4c9b6170925d646e
Daniel Fein <danieljfein@gmail.com>
2015-08-31 03:34:48 -0400

    DB Fix

--- db/migrate/20150826193834_add_timestamps_to_twitters.rb
+++ db/migrate/20150826193834_add_timestamps_to_twitters.rb
@@ -1,6 +1,5 @@
 class AddTimestampsToTwitters < ActiveRecord::Migration
   def change
-    add_column :validate_twitters, :created_at, :timestamp
-    add_column :validate_twitters, :updated_at, :timestamp
+
   end
 end
